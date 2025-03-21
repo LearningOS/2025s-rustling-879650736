4d1ed5d53e68e70290a461a9ce82e1e77f09f8f7
ssy <879650736@qq.com>
2025-03-21 08:09:11 +0000

    修复错误处理和泛型实现，更新函数返回类型，完善代码注释

--- exercises/strings/strings4.rs
+++ exercises/strings/strings4.rs
@@ -22,9 +22,9 @@ fn main() {
     string(String::from("hi"));
     string("rust is fun!".to_owned());
     string_slice("nice weather".into());
-    string_slice(format!("Interpolation {}", "Station"));
+    string(format!("Interpolation {}", "Station"));
     string_slice(&String::from("abc")[0..1]);
     string_slice("  hello there ".trim());
-    string_slice("Happy Monday!".to_string().replace("Mon", "Tues"));
-    string_slice("mY sHiFt KeY iS sTiCkY".to_lowercase());
+    string("Happy Monday!".to_string().replace("Mon", "Tues"));
+    string("mY sHiFt KeY iS sTiCkY".to_lowercase());
 }
