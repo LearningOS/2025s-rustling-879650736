60a7cf8361f936a4602fee26385afe60cbdbeb1c
ssy <879650736@qq.com>
2025-03-21 13:12:12 +0000

    更新多个文件中的未完成标记，修复宏和线程相关实现，完善错误处理和类型转换

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,26 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
+// I AM NOT DON
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.is_empty() {
+            return Person::default();
+        }
+        let parts: Vec<&str> = s.split(',').collect();
+        if parts.len() != 2 {
+            return Person::default();
+        }
+        let name = parts[0].to_string();
+        if name.is_empty() {
+            return Person::default();
+        }
+        let age = parts[1].parse::<usize>().unwrap_or_default();
+        if age == 0 {
+            return Person::default();
+        }
+        Person { name, age }
     }
 }
 
